44c88338782fb7d2abbc3786de6a2ac86db2930c
Phillip Chan <phillip.chan@atos.net>
2018-07-19 08:39:24 +0100

    Now pull message from lamda

--- src/main/java/net/atos/ocp/test/Application.java
+++ src/main/java/net/atos/ocp/test/Application.java
@@ -7,16 +7,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-    private static final String URL = "https://2k9ob97826.execute-api.us-east-2.amazonaws.com/default/TestLamda";
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -25,12 +21,5 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
-
-    private String getMessage(final String caseTypeId) {
-        final RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForEntity(URL, String.class).getBody();
-
-    }
-
 }
 
--- src/main/java/net/atos/ocp/test/HelloWildFlyController.java
+++ src/main/java/net/atos/ocp/test/HelloWildFlyController.java
@@ -2,13 +2,20 @@ package net.atos.ocp.test;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWildFlyController {
-
+    private static final String URL = "https://2k9ob97826.execute-api.us-east-2.amazonaws.com/default/TestLamda";
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+        return (getMessage());
+    }
+
+    private String getMessage() {
+        final RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(URL, String.class).getBody();
+
     }
 }
